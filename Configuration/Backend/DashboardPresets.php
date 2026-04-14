7b8df45724a54d557070dee6e724a5fae614761f
Konrad Michalik <km@move-elevator.de>
2026-04-14 17:20:08 +0200

    feat: add Services.yaml, dashboard preset and AJAX route

---
+++ Configuration/Backend/DashboardPresets.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the "solr_dashboard_widgets" TYPO3 CMS extension.
+ *
+ * (c) 2026 Konrad Michalik <hej@konradmichalik.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+    'solrOverview' => [
+        'title' => 'LLL:EXT:solr_dashboard_widgets/Resources/Private/Language/locallang.xlf:preset.solrOverview.title',
+        'description' => 'LLL:EXT:solr_dashboard_widgets/Resources/Private/Language/locallang.xlf:preset.solrOverview.description',
+        'iconIdentifier' => 'content-dashboard',
+        'defaultWidgets' => [
+            'solrDashboardWidgets.connectionStatus',
+            'solrDashboardWidgets.lastIndexingRun',
+            'solrDashboardWidgets.indexQueueStatus',
+            'solrDashboardWidgets.documentsInIndex',
+            'solrDashboardWidgets.indexQueueErrors',
+            'solrDashboardWidgets.searchStatistics',
+        ],
+        'showInWizard' => true,
+    ],
+];
