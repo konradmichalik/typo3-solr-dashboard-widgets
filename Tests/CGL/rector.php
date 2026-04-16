16967f777c42174562422aa4425f3d28b8988ced
Konrad Michalik <km@move-elevator.de>
2026-04-16 13:48:41 +0200

    fix: rector for typo3 version constraint in ext_emconf.php

--- Tests/CGL/rector.php
+++ Tests/CGL/rector.php
@@ -46,7 +46,7 @@ return RectorConfig::configure()
     ])
     ->withConfiguredRule(ExtEmConfRector::class, [
         ExtEmConfRector::PHP_VERSION_CONSTRAINT => '8.2.0-8.5.99',
-        ExtEmConfRector::TYPO3_VERSION_CONSTRAINT => '12.4.0-14.3.99',
+        ExtEmConfRector::TYPO3_VERSION_CONSTRAINT => '13.4.0-14.3.99',
         ExtEmConfRector::ADDITIONAL_VALUES_TO_BE_REMOVED => [],
     ])
     ->withSkip([
