c7315e17e4dbbedca6af2f9942d61b4c3fd5568a
Konrad Michalik <km@move-elevator.de>
2026-04-16 11:15:15 +0200

    feat: add individual colored icons per widget

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -11,6 +11,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
+use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
 use TYPO3\CMS\Core\Imaging\IconProvider\SvgSpriteIconProvider;
 
 return [
@@ -18,5 +19,41 @@ return [
         'provider' => SvgSpriteIconProvider::class,
         'source' => 'EXT:typo3_solr_dashboard_widgets/Resources/Public/Icons/Solr.svg',
         'sprite' => 'EXT:typo3_solr_dashboard_widgets/Resources/Public/Icons/sprites.svg#solr',
+    ],
+    'typo3-solr-dashboard-widgets-connection-status' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:typo3_solr_dashboard_widgets/Resources/Public/Icons/widget-connection-status.svg',
+    ],
+    'typo3-solr-dashboard-widgets-index-queue-status' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:typo3_solr_dashboard_widgets/Resources/Public/Icons/widget-index-queue-status.svg',
+    ],
+    'typo3-solr-dashboard-widgets-index-queue-errors' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:typo3_solr_dashboard_widgets/Resources/Public/Icons/widget-index-queue-errors.svg',
+    ],
+    'typo3-solr-dashboard-widgets-documents-in-index' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:typo3_solr_dashboard_widgets/Resources/Public/Icons/widget-documents-in-index.svg',
+    ],
+    'typo3-solr-dashboard-widgets-last-indexing-run' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:typo3_solr_dashboard_widgets/Resources/Public/Icons/widget-last-indexing-run.svg',
+    ],
+    'typo3-solr-dashboard-widgets-search-volume' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:typo3_solr_dashboard_widgets/Resources/Public/Icons/widget-search-volume.svg',
+    ],
+    'typo3-solr-dashboard-widgets-search-terms' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:typo3_solr_dashboard_widgets/Resources/Public/Icons/widget-search-terms.svg',
+    ],
+    'typo3-solr-dashboard-widgets-solr-health' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:typo3_solr_dashboard_widgets/Resources/Public/Icons/widget-solr-health.svg',
+    ],
+    'typo3-solr-dashboard-widgets-cache-hit-rates' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:typo3_solr_dashboard_widgets/Resources/Public/Icons/widget-cache-hit-rates.svg',
     ],
 ];
